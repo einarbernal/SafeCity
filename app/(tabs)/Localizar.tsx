83870b70e049c49f3f28f6d78a11d0a17ac0d2fe
leonel1236xd <leonelferrel03@gmail.com>
2025-05-20 19:32:36 -0400

    Menu del Ciudadano

--- app/(tabs)/Localizar.tsx
+++ app/(tabs)/Localizar.tsx
@@ -3,6 +3,7 @@ import { Stack } from 'expo-router';
 import React, { useRef, useState } from 'react';
 import { Dimensions, FlatList, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import MapView, { Callout, Marker } from 'react-native-maps';
+import HamburgerMenu from '../auth/MenuHamburguesa';
 
 const { width, height } = Dimensions.get('window');
 
@@ -81,6 +82,7 @@ export default function MapScreen() {
           headerTitleStyle: {
             fontWeight: 'bold',
           },
+          headerLeft: ()=> <HamburgerMenu/>
         }}
       />
 
